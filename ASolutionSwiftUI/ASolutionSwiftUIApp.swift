155f32aaa94221894b774cec7fee50cf223974f1
Farid Rzayev <FaridFr@code.edu.az>
2024-09-26 12:01:06 +0400

    Initial Commit

---
+++ ASolutionSwiftUI/ASolutionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASolutionSwiftUIApp.swift
+//  ASolutionSwiftUI
+//
+//  Created by Farid Rzayev on 26.09.24.
+//
+
+import SwiftUI
+
+@main
+struct ASolutionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
